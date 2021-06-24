aab92d921cd05c15e92668521c7c802beb3572e7
Stackbit Code Editor <projects@stackbit.com>
2021-06-24 07:14:38 +0000

    init.js: changed

--- js/init.js
+++ js/init.js
@@ -3,6 +3,3 @@
     This is where your custom site code goes.
     This script will be loaded on all pages and posts.
  */
-
- console.log('cl',document.querySelectorAll('.hero .copy')[0].innerText);
- throw 'bye';
